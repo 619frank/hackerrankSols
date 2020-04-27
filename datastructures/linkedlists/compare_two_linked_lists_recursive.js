e6b736bb9546b82d7687e7feabdd9608fa0fc7b7
Frank David <frank.mailservices@gmail.com>
2020-04-27 10:05:51 +0530

    Create compare_two_linked_lists_recursive.js

---
+++ datastructures/linkedlists/compare_two_linked_lists_recursive.js
@@ -0,0 +1,7 @@
+function CompareLists(llist1, llist2) {
+    if(llist1 === null && llist2 === null) return true
+    if(llist1 === null && llist2 !== null) return false
+    if(llist1 !== null && llist2 === null) return false
+    if(llist1.data !== llist2.data) return false    
+    return CompareLists(llist1.next, llist2.next)
+}
